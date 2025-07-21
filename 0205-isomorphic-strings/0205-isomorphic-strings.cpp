5f84a0686aa427a0a4f5008def35f6defee17071
natrajanmd-579 <natrajanmd579@gmail.com>
2025-07-21 23:28:04 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.57%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length()!=t.length()){return false;}
+        unordered_map<char,char>mapS,mapT;
+        for(int i=0;i<s.length();i++){
+            char c1=s[i];
+            char c2=t[i];
+        if(mapS.count(c1)){
+            if(mapS[c1]!=c2){return false;}
+        }else{
+            mapS[c1]=c2;
+        }
+
+        if(mapT.count(c2)){
+            if(mapT[c2]!=c1){return false;}
+        }else{
+            mapT[c2]=c1;
+        }
+    }
+    return true;
+    }
+};
