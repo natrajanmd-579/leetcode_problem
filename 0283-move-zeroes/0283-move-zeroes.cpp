7a271fc62b9a3554ec627a24c99fe7a0a15fc9fd
natrajanmd-579 <natrajanmd579@gmail.com>
2025-08-24 05:10:43 +0530

    Time: 0 ms (100%), Space: 25.1 MB (6.16%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+          vector<int>nonzero;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+        if(nums[i]!=0)
+        {
+            nonzero.push_back(nums[i]);
+        }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]==0)
+            {
+                nonzero.push_back(nums[i]);
+            }
+        }
+        nums=nonzero;
+    }
+};
