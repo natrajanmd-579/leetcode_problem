2caab87b187fe9312504d27125643b89519881c0
natrajanmd-579 <natrajanmd579@gmail.com>
2025-12-15 23:00:45 +0530

    Time: 0 ms (100%), Space: 8.1 MB (60.96%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,12 +2,6 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
        if(s.length()!=goal.length()){return false;}
-       for(int i=0;i<s.length();i++){
-            string rotate=s.substr(i)+s.substr(0,i);
-            if(rotate==goal){
-                return true;
-            }
-       }
-       return false; 
+       return (s+s).find(goal)!=string::npos;
     }
 };
