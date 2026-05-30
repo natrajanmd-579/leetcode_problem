93b7147aa5d0dc6eb5c356eb9ea028437f095dbc
natrajanmd-579 <natrajanmd579@gmail.com>
2026-05-30 22:16:04 +0530

    Time: 2 ms (72.59%), Space: 10.5 MB (39.83%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>>ans;
+
+    void solve(int row, int n, vector<string>& board,
+               vector<int>& col, vector<int>& diag1, vector<int>& diag2) {
+        if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+        for (int j = 0; j < n; j++) {
+            if (col[j] || diag1[row + j] || diag2[row - j + n - 1])
+                continue;
+            board[row][j] = 'Q';
+            col[j] = diag1[row + j] = diag2[row - j + n - 1] = 1;
+            solve(row + 1, n, board, col, diag1, diag2);
+            board[row][j] = '.';
+            col[j] = diag1[row + j] = diag2[row - j + n - 1] = 0;
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        vector<int> col(n, 0);
+        vector<int> diag1(2 * n, 0);
+        vector<int> diag2(2 * n, 0);
+
+        solve(0, n, board, col, diag1, diag2);
+
+        return ans;
+    }
+};
