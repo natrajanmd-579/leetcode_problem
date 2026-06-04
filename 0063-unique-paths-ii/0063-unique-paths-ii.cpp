aec9d9bd982b09f670c73333dc6842bb0a381113
natrajanmd-579 <natrajanmd579@gmail.com>
2026-06-04 20:07:04 +0530

    Time: 0 ms (100%), Space: 11.5 MB (84.46%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,32 +4,22 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<long long>> dp(m, vector<long long>(n, 0));
+        vector<long long> dp(n, 0);
 
-        // Start
-        if (grid[0][0] == 0) dp[0][0] = 1;
+        dp[0] = (grid[0][0] == 0) ? 1 : 0;
 
-        // First row
-        for (int j = 1; j < n; j++) {
-            if (grid[0][j] == 0)
-                dp[0][j] = dp[0][j - 1];
-        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
-        // First column
-        for (int i = 1; i < m; i++) {
-            if (grid[i][0] == 0)
-                dp[i][0] = dp[i - 1][0];
-        }
-
-        // Rest
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (grid[i][j] == 0) {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                if (grid[i][j] == 1) {
+                    dp[j] = 0;  // obstacle
+                }
+                else if (j > 0) {
+                    dp[j] += dp[j - 1];
                 }
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 };
