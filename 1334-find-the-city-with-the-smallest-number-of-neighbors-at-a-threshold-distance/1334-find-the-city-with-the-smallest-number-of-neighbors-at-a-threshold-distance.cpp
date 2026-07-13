adca7002f0e9f230fbc2a2b9f84e88d9f8a57217
natrajanmd-579 <natrajanmd579@gmail.com>
2026-07-13 11:25:01 +0530

    Time: 20 ms (28.26%), Space: 16.7 MB (71.47%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -18,7 +18,7 @@ public:
         for(int via=0;via<n;via++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
-                    if(dist[via][j]!=0 && dist[i][via]!=0){
+                    if(dist[via][j]!=1e9 && dist[i][via]!=1e9){
                         dist[i][j]=min(dist[i][j],dist[via][j]+dist[i][via]);
                     }
                 }
