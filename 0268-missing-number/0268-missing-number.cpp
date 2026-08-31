c95669a2956b8b00da1504c32447ab265417b9e8
natrajanmd-579 <natrajanmd579@gmail.com>
2026-08-31 23:48:39 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-       int missingNumber(vector<int>& nums) {
-       sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != i) {
-                return i;
-            }
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int ans=n;
+        for(int i=0;i<n;i++){
+            ans^=i;
+            ans^=nums[i];
         }
-        return nums.size();
+        return ans;
     }
 };
