493a1de5ddd2c2324a9884c3962972080a2f1209
natrajanmd-579 <natrajanmd579@gmail.com>
2026-09-09 23:34:14 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.72%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int a=n-999;
+        cout<<a;
+        if(a > 0){
+            return a;
+        }
+        else{
+            return 0;
+        }
+    }
+};
